df75f2194b26d3e84f2688bcdcc0da868faa66e2
zpneal <zpneal@msu.edu>
2022-12-20 14:25:59 -0500

    Update for 2022

--- xmas_movies.R
+++ xmas_movies.R
@@ -1,56 +1,46 @@
-rm(list=ls())
+#Load packages
 library(backbone)
 library(igraph)
 
-#### Visualization function ####
-draw.xmas.network <- function(net) {
-  set.seed(1225)
-  net <- delete.vertices(net, which(degree(net)==0))  #Remove isolates
-  
-  #Make the labels look nice
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "\\ ", "\n")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "\\.", "\n")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "A\n", "A ")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "An\n", "An ")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "\na\n", "\na ")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "The\n", " The ")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "\nthe\n", " the\n")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "\nand\n", " &\n")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "\nin\n", " in\n")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "\nof\n", " of\n")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "\nit", " it")
-  V(net)$name <- stringr::str_replace_all(V(net)$name, "is\n", " is ")
-  
-  #Color by community membership
-  community <- cluster_louvain(net)
-  V(net)$color <- community$membership              
-  V(net)[V(net)$color == 1]$color <- rgb(.77,.26,.27,.5)
-  V(net)[V(net)$color == 2]$color <- rgb(0,.53,.24,.5)
-  V(net)[V(net)$color == 3]$color <- rgb(.83,.68,.21,.5)
-  V(net)[V(net)$color == 4]$color <- rgb(.75,.75,.75,.5)
-  V(net)[V(net)$color == 5]$color <- "#235E6F80"
-  V(net)[V(net)$color == 6]$color <- "#8B008B80"
-  
-  #Adjust layout to highlight communities
-  weights <- ifelse(crossing(community, net), .45, .2)
-  layout <- layout_with_kk(net, weights = weights)
-  
-  plot(net, layout = layout, vertex.label.cex = .5, vertex.label.color = "black",
-       vertex.label.font = 2, vertex.label.family = "sans", vertex.frame.color = NA, vertex.size = 20)
-}
+#Import data
+B <- as.matrix(read.csv("xmas_movies.csv", row.names = 1, header = TRUE))
 
-#### Import Data ####
-B <- read.csv("xmas_movies.csv", row.names = 1, header = TRUE)
+#Extract signed backbone
+trope <- fdsm(t(B), trials = 50000, alpha = 0.2, class = "igraph", narrative = TRUE, signed = TRUE)
+trope <- delete.vertices(trope, which(degree(trope)==0))  #Remove isolates
 
-#### The movie network ####
-movie <- fdsm(B, trials = 50000, alpha = 0.15, class = "igraph", narrative = TRUE)
-pdf("movie.pdf")
-draw.xmas.network(movie)
-dev.off()
+#Layout
+positive <- delete_edges(trope, which(E(trope)$weight==-1))
+layout <- layout_with_kk(positive)
 
-#### The trope network ####
-trope <- fdsm(t(B), trials = 50000, alpha = 0.15, class = "igraph", narrative = TRUE)
+#Color
+E(trope)$edgecolor <- rgb(0,.53,.24,.75)
+E(trope)$edgecolor[which(E(trope)$weight==-1)] <- rgb(.77,.26,.27,.25)
+E(trope)$edgewidth <- 3
+E(trope)$edgewidth[which(E(trope)$weight==-1)] <- 1
+
+#Make the labels look nice
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\\ ", "\n")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\\.", "\n")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "A\n", "A ")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "An\n", "An ")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\na\n", "\na ")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "The\n", " The ")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\nthe\n", " the\n")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\nand\n", " &\n")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\nin\n", " in\n")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\nto\n", "  to\n")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\nof\n", " of\n")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\nit", " it")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "\nbe", " be")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "or\n", "or ")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, "is\n", " is ")
+V(trope)$name <- stringr::str_replace_all(V(trope)$name, " belief", "\nbelief")
+
+#Plot
 pdf("trope.pdf")
-draw.xmas.network(trope)
+plot(trope, layout = layout, 
+     vertex.label.cex = .5, vertex.label.color = "black", vertex.label.font = 2, vertex.label.family = "sans", 
+     vertex.frame.color = NA, vertex.size = 20, vertex.color = rgb(.83,.68,.21,.5),
+     edge.color = E(trope)$edgecolor, edge.width = E(trope)$edgewidth)
 dev.off()
-
